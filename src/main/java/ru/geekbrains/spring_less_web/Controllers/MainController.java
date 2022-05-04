d34ace2645dd40170636e734ede2a4683f760ee6
alekoChe <aleko2117bis@gmail.com>
2022-05-04 16:34:22 +0300

    удалил кое что

--- src/main/java/ru/geekbrains/spring_less_web/Controllers/MainController.java
+++ src/main/java/ru/geekbrains/spring_less_web/Controllers/MainController.java
@@ -10,31 +10,6 @@ import java.util.List;
 @RestController
 public class MainController {
 
-//    @Autowired
-//    private ClientRepository clientRepository;
-//
-//    @Autowired
-//    private ClientService service;
-//
-//    @GetMapping("/client/all")
-//    public List<Client> getTest(){
-//        return clientRepository.getAllClients();
-//    }
-//
-//    @GetMapping("/client/change_score")
-//    public void changeScore(@RequestParam Long clientId, @RequestParam Integer delta){
-//        service.changeScore(clientId, delta);
-//    }
-//
-//    @PostMapping("/client/add")
-//    public void addClientPost(@RequestBody Client client){
-//        clientRepository.addClient(client);
-//    }
-
-
-//    @Autowired
-//    private ProductRepository productRepository;
-
     @Autowired
     private ProductService prService;
 
@@ -62,8 +37,4 @@ public class MainController {
     public void deleteProductFromCart(@RequestParam Long id) {
         prService.deleteProductFromCart(id);
     }
-//    @GetMapping("cart/delete_product")
-//    public void deleteProductFromCart(@RequestParam Long productId) {
-//        prService.deleteProductFromCart(productId);
-//    }
 }
